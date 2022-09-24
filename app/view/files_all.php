01d6de416b5188a32541bef8d780d9a9192d47bb
Ajithkumar M <ajith721@gmail.com>
2022-09-24 17:18:37 +0530

    add header

--- app/view/files_all.php
+++ app/view/files_all.php
@@ -1,11 +1,22 @@
 <!DOCTYPE html>
-<html>
-<body>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Simple Tables</title>
 
-<h1>My first PHP page</h1>
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
+</head>
+
 
 <?php
-echo "<div class='card'>
+echo "<body class='hold-transition sidebar-mini'>
+<div class='card'>
 <div class='card-header'>
   <h3 class='card-title'>Responsive Hover Table</h3>
 
