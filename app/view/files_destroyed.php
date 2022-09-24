8ecd904a514f998545e65b925bef7db6312c73df
Ajithkumar M <ajith721@gmail.com>
2022-09-24 18:06:22 +0530

    update ui

--- app/view/files_destroyed.php
+++ app/view/files_destroyed.php
@@ -130,7 +130,7 @@
                                     </a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="app/view/files_all.php" class="nav-link">
+                                    <a href="app/view/files_destroyed.php" class="nav-link">
                                         <i class="far fa-circle text-warning"></i>
                                         <p>Distroyed Files</p>
                                     </a>
@@ -754,13 +754,7 @@
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6">
-                            <h1 class="m-0">Dashboard</h1>
-                        </div><!-- /.col -->
-                        <div class="col-sm-6">
-                            <ol class="breadcrumb float-sm-right">
-                                <li class="breadcrumb-item"><a href="#">Home</a></li>
-                                <li class="breadcrumb-item active">Dashboard v1</li>
-                            </ol>
+                            <h1 class="m-0">Destroyed Files</h1>
                         </div><!-- /.col -->
                     </div><!-- /.row -->
                 </div><!-- /.container-fluid -->
@@ -772,7 +766,6 @@
                 <div class="container-fluid">
                     <div class="card">
                         <div class="card-header">
-                            <h3 class="card-title">Destroyed Files</h3>
                             <div class="card-tools">
                                 <div class="input-group input-group-sm" style="width: 150px;">
                                     <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
